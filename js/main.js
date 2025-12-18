d61e95765b17e5abd681c959a8a0a524dfdddc1c
loxleyfashion <loxleyfashion@gmail.com>
2025-12-18 16:17:53 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,2 @@
-const fades = document.querySelectorAll('.fade');
 
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-    }
-  });
-}, { threshold: 0.2 });
-
-fades.forEach(el => observer.observe(el));
+// reserved for future animations
