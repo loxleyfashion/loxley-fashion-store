b7c4d8589c820ffc8a06f60f0686a877ed1b8119
loxleyfashion <loxleyfashion@gmail.com>
2025-12-19 12:32:30 +0530

    Create products.js

---
+++ js/products.js
@@ -0,0 +1,14 @@
+const PRODUCTS = [
+  {
+    id: "black-tee",
+    name: "Loxley Black Tee",
+    price: 799,
+    image: "blacktee.jpg"
+  },
+  {
+    id: "white-tee",
+    name: "Loxley White Tee",
+    price: 799,
+    image: "white-tee.jpg"
+  }
+];
