78cd6f269cf9732ba770f5686642d3a1284b4e22
loxleyfashion <loxleyfashion@gmail.com>
2025-12-19 14:30:02 +0530

    Update products.js

--- js/products.js
+++ js/products.js
@@ -3,24 +3,22 @@ const products = [
     id: "loxley-black-tee",
     name: "Loxley Premium Black Tee",
     price: 799,
-    category: "tshirts",
-    image: "images/black-tee.jpg",
+    images: [
+      "images/black-tee-1.jpg",
+      "images/black-tee-2.jpg",
+      "images/black-tee-3.jpg"
+    ],
     tag: "BESTSELLER"
   },
+
   {
     id: "korean-jogger-black",
     name: "Korean Joggers – Black",
     price: 1599,
-    category: "joggers",
-    image: "images/jogger-black.jpg",
+    images: [
+      "images/jogger-black-1.jpg",
+      "images/jogger-black-2.jpg"
+    ],
     tag: "TRENDING"
-  },
-  {
-    id: "oversized-hoodie",
-    name: "Oversized Hoodie – Grey",
-    price: 1899,
-    category: "hoodies",
-    image: "images/hoodie-grey.jpg",
-    tag: "NEW"
   }
 ];
